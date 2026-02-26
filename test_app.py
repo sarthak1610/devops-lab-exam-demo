fdf38ecbcd6f3c13d5b4589a9ed08b623fbc0e1d
Sarthak <131865702+sarthak1610@users.noreply.github.com>
2026-02-26 11:16:06 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from app import factorial
+import pytest
+
+def test_factorial_positive():
+    assert factorial(5) == 120
+
+def test_factorial_zero():
+    assert factorial(0) == 1
+
+def test_factorial_one():
+    assert factorial(1) == 1
